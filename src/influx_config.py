99dd0f717f5581f54e2b8edf6a59789ee5b1107c
nhatsinh97 <55687877+nhatsinh97@users.noreply.github.com>
2025-06-22 20:55:37 +0700

    Use env vars for InfluxDB

--- src/influx_config.py
+++ src/influx_config.py
@@ -1,10 +1,14 @@
 # influx_config.py
 from influxdb import InfluxDBClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = InfluxDBClient(
-    host='localhost',
-    port=8086,
-    # username='',   # nếu không đặt auth thì để trống
-    # password='',   # nếu không đặt auth thì để trống
-    database='ats_data'
+    host=os.getenv("INFLUXDB_HOST", "localhost"),
+    port=int(os.getenv("INFLUXDB_PORT", 8086)),
+    username=os.getenv("INFLUXDB_USERNAME"),
+    password=os.getenv("INFLUXDB_PASSWORD"),
+    database=os.getenv("INFLUXDB_DATABASE", "ats_data"),
 )
